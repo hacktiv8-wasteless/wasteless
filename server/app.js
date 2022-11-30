60cbdcd5511d794f4aaeac7744dd6dfaaa9a9290
ionnotion <adryan.rahadianto@gmail.com>
2022-11-30 17:44:41 +0700

    init: init server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,45 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { mongoConnect } = require("./config/mongo");
+
+const typeDefs = `#graphql
+	type User {
+    _id:String
+    username:String
+    email:String!
+    password:String!
+    phoneNumber:String
+    address:String
+  }
+
+  type Query {
+	query1:User
+  }
+
+  type Mutation {
+	mutation1:User
+  }
+`;
+
+const resolvers = {
+	Query: {
+		query1: () => {},
+	},
+	Mutation: {
+		mutation1: () => {},
+	},
+};
 
 const server = new ApolloServer({
-	typeDefs: [],
-	resolvers: [],
+	typeDefs,
+	resolvers,
 });
 
-startStandaloneServer(server, {
-	listen: { port: process.env.PORT || 4000 },
-}).then(({ url }) => {
-	console.log(`🚀  Server ready at: ${url}`);
-});
+(async () => {
+	await mongoConnect();
+	startStandaloneServer(server, {
+		listen: { port: process.env.PORT || 4000 },
+	}).then(({ url }) => {
+		console.log(`🚀  Server ready at: ${url}`);
+	});
+})();
