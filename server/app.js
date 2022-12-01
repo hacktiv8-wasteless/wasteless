fe023f7d83caea39afc0f87a3dee0903c02b806b
ionnotion <adryan.rahadianto@gmail.com>
2022-12-01 18:51:18 +0700

    feat: add test client setting, fix context, add directive and schema skeleton

--- server/app.js
+++ server/app.js
@@ -1,50 +1,29 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { mongoConnect } = require("./config/mongo");
 const userSchema = require("./schemas/usersSchema");
-const typeDefs = `#graphql
-	type User {
-    _id:String
-    username:String
-    email:String!
-    password:String!
-    phoneNumber:String
-    address:String
-  }
 
-  type Query {
-	query1:User
-  }
-
-  type Mutation {
-	mutation1:User
-  }
-`;
-
-const resolvers = {
-	Query: {
-		query1: () => {},
-	},
-	Mutation: {
-		mutation1: () => {},
-	},
-};
-
-const server = new ApolloServer({
+const schema = makeExecutableSchema({
 	typeDefs: [userSchema.typeDefs],
 	resolvers: [userSchema.resolvers],
 });
 
-(async () => {
-	await mongoConnect();
+const server = new ApolloServer({ schema });
+
+async function start(env) {
+	await mongoConnect(env);
 	startStandaloneServer(server, {
 		listen: { port: process.env.PORT || 4000 },
 		context: async ({ req }) => {
-			const token = req.headers.authorization || null;
-			const user = getUser(token);
-			return { user };
+			const token = req.headers.authorization || "";
+			return { token };
 		},
 	}).then(({ url }) => {
 		console.log(`🚀  Server ready at: ${url}`);
 	});
-})();
+};
+
+start("dev")
+
+module.exports = start
