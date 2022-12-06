9e962daa45b045dda0652be7f0eefb6faa95b285
FadyllahF <farhanfady1@gmail.com>
2022-12-06 15:23:39 +0700

    Fixing payment Screen

--- client/client-mobile/src/screens/XenditScreen.js
+++ client/client-mobile/src/screens/XenditScreen.js
@@ -1,17 +1,17 @@
 import { WebView } from "react-native-webview";
 import { SafeAreaView, View, Text } from "react-native";
 import { Button } from "native-base";
-
+import { useNavigation } from "@react-navigation/native";
 import { useMutation } from "@apollo/client";
 import { TopUp } from "../query/TopUp";
 
-export default function XenditPayment({ navigation, route }) {
+export default function XenditPayment({}) {
   //   const { token, invoiceId } = route.params;
 
   //   const [createInvoice, { loading, error, data }] = useMutation(TopUp);
 
   //   const uri = `https://checkout-staging.xendit.co/web/638edd5c1cea5504be4d2826`;
-
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
@@ -19,7 +19,9 @@ export default function XenditPayment({ navigation, route }) {
           uri: "https://checkout-staging.xendit.co/web/638edd5c1cea5504be4d2826",
         }}
       />
-      <Button onPress={() => console.log("hello world")}>Pay Now</Button>
+      <Button onPress={() => navigation.navigate("MyProfile")}>
+        Top Up Now
+      </Button>
     </SafeAreaView>
   );
 }
